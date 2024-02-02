ff537af93550c20af78475d62a91dbf5ba1340b3
Taras Krasitskyi <taras.krasitsky@gmail.com>
2024-02-02 12:16:44 +0200

    urlReader based on Reader and Writer

---
+++ src/main/java/org/krasitskyi/urlreader/ReaderAppl.java
@@ -0,0 +1,23 @@
+package org.krasitskyi.urlreader;
+
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+
+public class ReaderAppl {
+    public static void main(String[] args) throws IOException {
+//        Reader urlReader = new URLReader("https://uk.wikipedia.org/wiki/%D0%93%D0%BE%D0%BB%D0%BE%D0%B2%D0%BD%D0%B0_%D1%81%D1%82%D0%BE%D1%80%D1%96%D0%BD%D0%BA%D0%B0");
+//        StringBuilder builder = new StringBuilder();
+//        char[] buffer = new char[1024];
+//        int charsRead;
+//        while ((charsRead = urlReader.read(buffer, 0, buffer.length)) != -1) {
+//            builder.append(buffer, 0, charsRead);
+//        }
+//        FileWriter fileWriter = new FileWriter("src/main/resources/index.html");
+//        fileWriter.write(builder.toString());
+//        fileWriter.close();
+        URLReader urlReader = new URLReader("https://uk.wikipedia.org/wiki/%D0%93%D0%BE%D0%BB%D0%BE%D0%B2%D0%BD%D0%B0_%D1%81%D1%82%D0%BE%D1%80%D1%96%D0%BD%D0%BA%D0%B0");
+        urlReader.writeURLContentToFile("src/main/resources/index.html");
+    }
+}
